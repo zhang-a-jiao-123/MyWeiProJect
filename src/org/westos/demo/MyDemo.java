1e19c349c045eec8d6fe8cddb8605247bc9cbb06
zhangajiao <m17853149561@163.com>
2019-09-11 17:13:38 +0800

    第一次提交

---
+++ src/org/westos/demo/MyDemo.java
@@ -0,0 +1,15 @@
+package org.westos.demo;
+
+/**
+ * @Author: lqn
+ * @CreateTime: 2019-09-11 13:46
+ * @Description:西部开源教育科技有限公司
+ */
+public class MyDemo {
+    public static void main(String[] args) {
+        System.out.println("我编写了第一个功能");
+        System.out.println("我编写了第二个功能");
+        System.out.println("不可以随便删");
+        System.out.println("非要删除");
+    }
+}
